a28a65ca03ea81da75c803377689e5db71eb9327
中川　友太朗 <nakagawayuutaakira@nakagawayuutaakiranoMacBook-ea.local>
2020-06-04 20:09:40 +0900

    tomo

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -18,8 +18,8 @@ class ImageUploader < CarrierWave::Uploader::Base
    def default_url(*args)
   #   # For Rails 3.1+ asset pipeline compatibility:
    #ActionController::Base.helpers.asset_path("fallback/" + [version_name, "default.png"].compact.join('_'))
-      #"default.png"
-      "/images/" + [version_name, "default.png"].compact.join('_')
+      "default.png"
+      #"/images/" + [version_name, "default.png"].compact.join('_')
 
   #   "/images/fallback/" + [version_name, "default.png"].compact.join('_')
    end
