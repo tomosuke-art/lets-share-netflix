d30f872a9b84584d9ea287c0b530c3396a164347
tomosuke-art <tomo1130taro@icloud.com>
2020-08-21 19:31:57 +0900

    update serialize JS

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -38,7 +38,7 @@ class PostsController < ApplicationController
 
   def show
    @post=Post.find_by(id: params[:id])
-   @user=User/find_by(id: @post.user_id)
+   @user=User.find_by(id: @post.user_id)
   end
 
   def new
